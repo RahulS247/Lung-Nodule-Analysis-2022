40f1e46560e2a673f1bc1d3dec7420e5da45413c
phigjm <phigjm@gmail.com>
2022-05-26 00:52:23 +0200

    batch script

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#Set job requirements
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=18
+#SBATCH --gpus=1
+#SBATCH --partition=gpu
+#SBATCH --time=03:00:00
+ 
+module load 2021
+module load TensorFlow/2.6.0-foss-2021a-CUDA-11.3.1
+
+#source ~/venv/bin/activate
+python train.py
