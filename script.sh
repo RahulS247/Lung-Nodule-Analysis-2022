11581b77e1d3026d0fb8e2eb8d80f655efa996d5
phigjm <phigjm@gmail.com>
2022-06-10 11:55:55 +0200

    script try

--- script.sh
+++ script.sh
@@ -5,10 +5,11 @@
 #SBATCH --cpus-per-task=18
 #SBATCH --gpus=1
 #SBATCH --partition=gpu
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
  
 #Loading modules
-module purge 
-module load 2021 Python/3.9.5-GCCcore-10.3.0 
-pip uninstall tensorflow
+module load 2021 Python/3.9.5-GCCcore-10.3.0
+module load TensorFlow/2.6.0-foss-2021a-CUDA-11.3.1
+
+
 python train.py
